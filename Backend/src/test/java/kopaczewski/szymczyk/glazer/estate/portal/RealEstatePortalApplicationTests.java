9b99fed959ca6c6e6cb3fc486adb2c6791092432
danielglazer26 <danielglazer200026@gmail.com>
2022-11-14 23:04:16 +0100

    Add projects

---
+++ Backend/src/test/java/kopaczewski/szymczyk/glazer/estate/portal/RealEstatePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package kopaczewski.szymczyk.glazer.estate.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstatePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
