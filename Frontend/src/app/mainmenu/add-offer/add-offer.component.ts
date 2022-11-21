0be12cd45f05b2d5cc1820713ca46b05a78521f2
danielglazer26 <danielglazer200026@gmail.com>
2022-11-21 23:51:15 +0100

    Resolve conflict of forms

--- Frontend/src/app/mainmenu/add-offer/add-offer.component.ts
+++ Frontend/src/app/mainmenu/add-offer/add-offer.component.ts
@@ -1,34 +1,44 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {AnnouncementFormGroup} from "./announcement-form";
 
 @Component({
   selector: 'app-add-offer',
   templateUrl: './add-offer.component.html',
   styleUrls: ['./add-offer.component.css']
 })
-export class AddOfferComponent implements OnInit {
+export class AddOfferComponent {
 
-  rent: boolean = true
+  profileForm!: FormGroup;
+
+  type: boolean = true
   isAddEnable: boolean = true
 
-  constructor() {
+  constructor(fb: FormBuilder) {
+    this.profileForm = new AnnouncementFormGroup(fb)
   }
 
-  ngOnInit(): void {
-  }
 
   onTypeChange(type: number) {
-    this.rent = (type == 0)
+    this.type = (type == 0)
+    let jsonObject = JSON.parse(JSON.stringify(this.profileForm?.value, undefined, 4));
+    jsonObject.announcementType = this.type
+    // @ts-ignore
+    this.profileForm.patchValue(JSON.stringify(jsonObject))
   }
 
   fileInputChange(fileInputEvent: any) {
     console.log(fileInputEvent.target.files[0]);
   }
 
-  getIsAddEnable(){
+  getIsAddEnable() {
     return this.isAddEnable
   }
 
-  test() {
+  onSubmit() {
+   /* let json = JSON.stringify(this.profileForm?.value, undefined, 4);
+    console.log(json)*/
+
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
