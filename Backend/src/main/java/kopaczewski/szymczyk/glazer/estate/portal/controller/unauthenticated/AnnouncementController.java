a4ebf13bcf371c812c99a2b36e597e1755e5235c
Mikolaj Szymczyk <mikolaj.szymczyk.99@gmail.com>
2022-11-21 21:04:02 +0100

    add endpoint which listing Announcements with filters

---
+++ Backend/src/main/java/kopaczewski/szymczyk/glazer/estate/portal/controller/unauthenticated/AnnouncementController.java
@@ -0,0 +1,31 @@
+package kopaczewski.szymczyk.glazer.estate.portal.controller.unauthenticated;
+
+import kopaczewski.szymczyk.glazer.estate.portal.database.services.AnnouncementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public/")
+public class AnnouncementController {
+    private final AnnouncementService service;
+
+    @Autowired
+    public AnnouncementController(AnnouncementService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/announcements/")
+    public ResponseEntity<?> filerAnnouncements(
+            @RequestParam(required = false) Integer minCost, @RequestParam(required = false) Integer maxCost,
+            @RequestParam(required = false) Double minArea, @RequestParam(required = false) Double maxArea,
+            @RequestParam(required = false) Integer roomNumber, @RequestParam(required = false) String city,
+            @RequestParam(required = false) String street, @RequestParam(required = false) Integer apartmentNumber,
+            @RequestParam Integer limit, @RequestParam Integer offset) {
+        var list = service.getFiltered(minCost, maxCost, minArea, maxArea, roomNumber, city, street, apartmentNumber, limit, offset);
+        return ResponseEntity.ok().body(list);
+    }
+}
