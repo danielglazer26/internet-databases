c12753591bb52432ccdef70d615c81a4cf134db8
jarekopaczewski <jarekopaczewski@protonmail.com>
2022-11-27 22:15:48 +0100

    sdf

--- Backend/src/main/java/kopaczewski/szymczyk/glazer/estate/portal/database/TestDatabase.java
+++ Backend/src/main/java/kopaczewski/szymczyk/glazer/estate/portal/database/TestDatabase.java
@@ -1,9 +1,10 @@
 package kopaczewski.szymczyk.glazer.estate.portal.database;
 
 import kopaczewski.szymczyk.glazer.estate.portal.database.model.announcement.Announcement;
+import kopaczewski.szymczyk.glazer.estate.portal.database.model.announcement.AnnouncementType;
+import kopaczewski.szymczyk.glazer.estate.portal.database.model.announcement.ProvinceNameEnum;
 import kopaczewski.szymczyk.glazer.estate.portal.database.repositories.AnnouncementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -20,10 +21,10 @@ public class TestDatabase {
 
     @PostConstruct
     public void start() {
-        for( long i = 1L; i < 4000; i++){
-            announcementRepository.save(new Announcement(i,
-                    "Apartament B z tarasem w budynku z 2022 r", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in erat nibh. Curabitur vel tincidunt libero. Curabitur scelerisque fermentum justo nec auctor. Proin lacus ipsum, imperdiet nec purus scelerisque, fermentum auctor lacus. ", "login", (int)i,
-                    "ul. Węgliniecka", "Wrocław", (int)i * 100, (int)i, (int)i, (int)i, (double)i));
+        for (long i = 1L; i < 4000; i++) {
+            announcementRepository.save(new Announcement(
+                    i, "Apartament B z tarasem w budynku z 2022 r", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in erat nibh. Curabitur vel tincidunt libero. Curabitur scelerisque fermentum justo nec auctor. Proin lacus ipsum, imperdiet nec purus scelerisque, fermentum auctor lacus. ", AnnouncementType.RENTAL, "login",
+                    String.valueOf(i), "ul. Węgliniecka", "Wrocław", ProvinceNameEnum.KUYAVIAN_POMERANIAN, (int) i * 100, (int) i, (int) i, (int) i, (double) i));
         }
     }
 }
--- Frontend/src/app/mainmenu/offers/offer.model.ts
+++ Frontend/src/app/mainmenu/offers/offer.model.ts
@@ -1,25 +1,29 @@
 export class Offer {
   public title: string;
   public additionalDescription: string;
+  public announcementType: number;
   public ownerLogin: string;
   public apartmentNumber: number;
   public street: string;
   public city: string;
+  public province: number;
   public costPerMonth: number;
   public rent: number;
   public deposit: number;
   public roomNumber: number;
   public area: number;
 
-  constructor(title: string, additionalDescription: string, ownerLogin: string,
-              apartmentNumber: number, street: string, city: string, costPerMonth: number,
+  constructor(title: string, additionalDescription: string, announcementType: number, ownerLogin: string,
+              apartmentNumber: number, street: string, city: string, province: number, costPerMonth: number,
               rent: number, deposit: number, roomNumber: number, area: number) {
     this.title = title;
     this.additionalDescription = additionalDescription;
+    this.announcementType = announcementType
     this.ownerLogin = ownerLogin;
     this.apartmentNumber = apartmentNumber;
     this.street = street;
     this.city = city;
+    this.province = province
     this.costPerMonth = costPerMonth;
     this.rent = rent;
     this.deposit = deposit;
