975f03103daca8e38d867ec3d31e6108d5ae085e
Mikolaj Szymczyk <mikolaj.szymczyk.99@gmail.com>
2022-11-27 21:41:57 +0100

    add province

---
+++ Backend/src/main/java/kopaczewski/szymczyk/glazer/estate/portal/database/model/announcement/ProvinceNameEnum.java
@@ -0,0 +1,25 @@
+package kopaczewski.szymczyk.glazer.estate.portal.database.model.announcement;
+
+public enum ProvinceNameEnum {
+    LOWER_SILESIAN("dolnośląskie"),
+    KUYAVIAN_POMERANIAN("kujawsko-pomorskie"),
+    LUBLIN("lubelskie"),
+    LUBUSZ("lubuskie"),
+    LODZ("łódzkie"),
+    LESSER_POLAND("małopolskie"),
+    MASOVIAN("mazowieckie"),
+    OPOLE("opolskie"),
+    SUBCARPATHIAN("podkarpackie"),
+    PODLASKIE("podlaskie"),
+    POMERANIAN("pomorskie"),
+    SILESIAN("śląskie"),
+    HOLY_CROSS("świętokrzyskie"),
+    WARMIAN_MASURIAN("warmińsko-mazurskie"),
+    GREATER_POLAND("wielkopolskie"),
+    WEST_POMERANIAN("zachodniopomorskie");
+    final String  polishName;
+
+    ProvinceNameEnum(String s) {
+        polishName=s;
+    }
+}
