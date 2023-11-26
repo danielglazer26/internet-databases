38be4f30383c35ca2d69121d3f696a093ff1c1ce
Daniel Glazer <danielglazer200026@gmail.com>
2023-11-26 17:41:06 +0100

    Delete authenticated

--- src/main/java/bednarz/glazer/iwaniak/estate/portal/connection/settings/WebControllerConfig.java
+++ src/main/java/bednarz/glazer/iwaniak/estate/portal/connection/settings/WebControllerConfig.java
@@ -41,7 +41,7 @@ public class WebControllerConfig {
                 .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requestMatcherRegistry -> requestMatcherRegistry
-                        .requestMatchers("/api/auth/**", "/public/**", "/authenticated/**").permitAll()
+                        .requestMatchers("/api/auth/**", "/public/**").permitAll()
                         .anyRequest().authenticated()
                 ).authenticationProvider(authenticationProvider())
                 .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
