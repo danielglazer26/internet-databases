e931d57e0828a18f3c582c5e2643735c02c86360
Daniel Glazer <danielglazer200026@gmail.com>
2023-12-03 23:29:14 +0100

    Memory optimization v1

--- src/main/java/bednarz/glazer/iwaniak/estate/portal/database/TestDatabase.java
+++ src/main/java/bednarz/glazer/iwaniak/estate/portal/database/TestDatabase.java
@@ -36,7 +36,7 @@ public class TestDatabase {
     private static final double MIN_AREA = 50.5;
     private static final double MAX_AREA = 600.0;
     private static final int MIN_PHOTOS_PER_ANNOUNCEMENT = 2;
-    private static final int MAX_PHOTOS_PER_ANNOUNCEMENT = 5;
+    private static final int MAX_PHOTOS_PER_ANNOUNCEMENT = 3;
 
     private final AnnouncementRepository announcementRepository;
 
